31424a5d1f1ba9de2961fea9117f6b762b2a4b82
Elwinc2799 <elwinczh@gmail.com>
2021-03-24 04:07:19 +0000

    24-3-2021: Added reminder function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
 import os
+import datetime
+import asyncio
+import pytz
 
 client = discord.Client()
 
@@ -11,7 +14,34 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  if message.content.startwith('$hello'):
+  if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
+
+@client.event
+async def set_course_reminder(message):
+  if message.content.startswith('$set'):
+    await message.channel.send('Set')
+
+@client.event
+async def reminder():
+  while (True):
+    await client.wait_until_ready()
+    tz_KL = pytz.timezone('Asia/Kuala_Lumpur')
+    c_time = datetime.datetime.now(tz_KL).strftime("%Y-%m-%d %H:%M:%S")
+    temp = datetime.datetime(year=2021, month=3, day=24, hour=11, minute=0, second=0)
+    temp = temp - datetime.timedelta(minutes = 10)
+    r_time = temp.strftime("%Y-%m-%d %H:%M:%S")
+    print(c_time)
+    print(temp)
+    print(r_time)
+    channel = discord.utils.get(client.guilds[0].channels,name="time-schedule-channel")
+    user = "everyone"
+    if (r_time < c_time):
+      message = f"Left 10 min until {c_time}! Ready for CPC251!! @{user}."
+      await channel.send(message)
+      break;
+    await asyncio.sleep(5)
+
+client.loop.create_task(reminder())
 
 client.run(os.getenv('TOKEN'))
