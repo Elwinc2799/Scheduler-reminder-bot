f1e6f0acec186c1979e502af54035ab0e0e13040
Elwinc2799 <elwinczh@gmail.com>
2021-03-26 11:30:03 +0800

    Version 1.0.1 : Bug fixes

--- main.py
+++ main.py
@@ -53,54 +53,54 @@ async def set_course_reminder(message):
 
 @client.event
 async def reminder():
-  while True:
-    await client.wait_until_ready()
-    db_connection = create_connection('reminder.db')
-    timetable = select_all_tasks(db_connection)
-    for row in timetable:
-        y=0
-        for x in row:
-            if y==1:
-                temp_course_name = x
-            elif y==3:
-                tz_KL = pytz.timezone('Asia/Kuala_Lumpur')
-                temp = datetime.datetime.now(tz_KL) + timedelta(hours=0, minutes=10)
-                temp = temp.strftime("%H:%M:%S")
-                if x > temp:
-                    temp_time = x
-                else:
-                    temp_time = 0
-            elif y==4:
-                temp_target = x
-            y=y+1
-        while True:
-          if temp_time != 0:
-              tz_KL = pytz.timezone('Asia/Kuala_Lumpur')
-              c_time = datetime.datetime.now(tz_KL).strftime("%H:%M:%S")
-              date_time_obj = datetime.datetime.strptime(temp_time, '%H:%M:%S')
-              date_time_obj = date_time_obj - datetime.timedelta(minutes=10)
-              r_time = date_time_obj.strftime("%H:%M:%S")
-              print(c_time)
-              print(r_time)
-              channel = discord.utils.get(client.guilds[0].channels, name="time-schedule-channel")
-              if r_time < c_time:
-                message = temp_target + "\nReminder! " + temp_course_name + " is going to start in 10mins!"
-                print(message)
-                await channel.send(message)
-                break
-              else:
-                await asyncio.sleep(5)
-          else:
-              break
-    tempDay = str(datetime.datetime.now().weekday())
-    if (tempDay >= 0 and tempDay <5):
-      print ("Monday to Friday")
-      await asyncio.sleep(3600) 
-    else:
-      print("Saturday and Sunday")
-      await asyncio.sleep(21600)
+    while True:
+        await client.wait_until_ready()
+        tempDay = datetime.datetime.now().weekday()
+        if 0 <= tempDay < 5:
+            print("Monday to Friday")
+            db_connection = create_connection('reminder.db')
+            timetable = select_all_tasks(db_connection)
+            for row in timetable:
+                y = 0
+                for x in row:
+                    if y == 1:
+                        temp_course_name = x
+                    elif y == 3:
+                        tz_KL = pytz.timezone('Asia/Kuala_Lumpur')
+                        temp = datetime.datetime.now(tz_KL) + timedelta(hours=0, minutes=10)
+                        temp = temp.strftime("%H:%M:%S")
+                        if x > temp:
+                            temp_time = x
+                        else:
+                            temp_time = 0
+                    elif y == 4:
+                        temp_target = x
+                    y = y + 1
+                while True:
+                    if temp_time != 0:
+                        tz_KL = pytz.timezone('Asia/Kuala_Lumpur')
+                        c_time = datetime.datetime.now(tz_KL).strftime("%H:%M:%S")
+                        date_time_obj = datetime.datetime.strptime(temp_time, '%H:%M:%S')
+                        date_time_obj = date_time_obj - timedelta(minutes=10)
+                        r_time = date_time_obj.strftime("%H:%M:%S")
+                        print(c_time)
+                        print(r_time)
+                        channel = discord.utils.get(client.guilds[0].channels, name="time-schedule-channel")
+                        if r_time < c_time:
+                            message = temp_target + "\nReminder! " + temp_course_name + " is going to start in 10mins!"
+                            print(message)
+                            await channel.send(message)
+                            break
+                        else:
+                            await asyncio.sleep(5)
+                    else:
+                        break
+            await asyncio.sleep(5)
+        else:
+          print("Saturday and Sunday")
+          await asyncio.sleep(5)
 
 client.loop.create_task(reminder())
 
-keep_alive()
+#keep_alive()
 client.run(botToken)
